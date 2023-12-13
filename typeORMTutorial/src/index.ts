e4452a7c7aec9e61a98f349daf33f5f6e8d78ec8
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-12-13 16:54:05 +0100

    creating transactions

--- typeORMTutorial/src/index.ts
+++ typeORMTutorial/src/index.ts
@@ -2,6 +2,15 @@ import { createConnection } from 'typeorm'
 import { Client } from './entity/Client'
 import { Banker } from './entity/Banker'
 import { Transaction } from './entity/Transaction'
+import express from 'express'
+import { createClientRouter } from './routes/create-client'
+import { createBankerRouter } from './routes/create_banker'
+import { connectBankerToClientRouter } from './routes/connect_bank_to_client'
+import { createTransactionRouter } from './routes/create_transactions'
+import { deleteClientRouter } from './routes/delete_client'
+import { fetchClientsRouter } from './routes/fetch_client'
+
+const app = express()
 
 const main = async () => {
   try {
@@ -15,6 +24,18 @@ const main = async () => {
       entities:[Client,Banker,Transaction],
       synchronize:true
     })
+    app.use(express.json())
+    app.use(express.urlencoded({extended:false}))
+    app.use(createClientRouter);
+		app.use(createBankerRouter);
+		app.use(connectBankerToClientRouter);
+		app.use(createTransactionRouter);
+		app.use(deleteClientRouter);
+		app.use(fetchClientsRouter);
+
+		app.listen(8080, () => {
+			console.log('Now running on port 8080');
+		});
     console.log('Connected to postgresql')
   } catch (error) {
     console.log(error)
