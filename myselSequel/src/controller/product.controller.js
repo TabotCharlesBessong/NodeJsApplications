363270f68604fb85b037a8a9aaf668bc528fe032
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-04-17 21:06:39 +0100

    getting single product, update and deleting product

--- myselSequel/src/controller/product.controller.js
+++ myselSequel/src/controller/product.controller.js
@@ -1,3 +1,4 @@
+const { where } = require("sequelize");
 const db = require("../models");
 
 const Product = db.products;
@@ -12,15 +13,43 @@ const addProduct = async (req, res) => {
     published: req.body.published,
   };
 
-  const product = await Product.create(info)
-  res.status(200).send(product)
-  console.log(product)
+  const product = await Product.create(info);
+  res.status(200).send(product);
+  console.log(product);
 };
 
 // 2 getting all products
-const getAllProducts = async (req,res) => {
-  let products = await Product.findAll()
-  res.status(201).send(products)
-}
+const getAllProducts = async (req, res) => {
+  let products = await Product.findAll();
+  res.status(201).send(products);
+};
 
-module.exports = {addProduct,getAllProducts}
+// 3 get a single product
+const getOneProduct = async (req, res) => {
+  const id = req.params.id;
+  const product = await Product.findOne({ where: { id } });
+  res.status(200).send(product);
+};
+
+// 4. update Product
+
+const updateProduct = async (req, res) => {
+  const id = req.params.id;
+  const product = await Product.update(req.body, { where: { id: id } });
+  res.status(200).send(product);
+};
+
+// 5. delete product
+const deleteProduct = async (req, res) => {
+  const id = req.params.id;
+  await Product.destroy({ where: { id } });
+  res.status(200).send("Product deleted successfully");
+};
+
+module.exports = {
+  addProduct,
+  getAllProducts,
+  getOneProduct,
+  updateProduct,
+  deleteProduct,
+};
--- myselSequel/src/router/product.router.js
+++ myselSequel/src/router/product.router.js
@@ -1,9 +1,18 @@
-const express = require("express")
-const { addProduct, getAllProducts } = require("../controller/product.controller")
+const express = require("express");
+const {
+  addProduct,
+  getAllProducts,
+  getOneProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controller/product.controller");
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/new",addProduct)
-router.get("/all",getAllProducts)
+router.post("/new", addProduct);
+router.get("/all", getAllProducts);
+router.get("/:id", getOneProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
-module.exports = router
+module.exports = router;
