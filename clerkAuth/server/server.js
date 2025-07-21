c23cc8579f25050c163fc42d92c319632389bee0
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-07-21 12:33:52 +0100

    querying users

--- clerkAuth/server/server.js
+++ clerkAuth/server/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
-const cors = require('cors');
-const {clerkClient} = require("@clerk/express")
+const express = require("express");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const { clerkClient } = require("@clerk/express");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,10 +9,15 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-  const {data,totalCount} = await clerkClient.users.getUserList()
+app.get("/", async (req, res) => {
+  const { data, totalCount } = await clerkClient.users.getUserList({
+    limit: 10,
+    orderBy: "+first_name",
+    query: "",
+    pageSize: 10,
+  });
   // res.send('Hello, World!');
-  res.json({data,total:totalCount})
+  res.json({ data, total: totalCount });
 });
 
 app.listen(port, () => {
