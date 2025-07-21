7a24f9f8b0b4e12f4d1ccf8ddede81267120e9cf
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-07-21 13:36:40 +0100

    banning, unbanning, locking and unlocking users

--- clerkAuth/server/server.js
+++ clerkAuth/server/server.js
@@ -10,21 +10,39 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  // const { data, totalCount } = await clerkClient.users.getUserList({
-  //   limit: 10,
-  //   orderBy: "+first_name",
-  //   query: "",
-  //   pageSize: 10,
-  // });
+  const { data, totalCount } = await clerkClient.users.getUserList({
+    limit: 10,
+    orderBy: "+first_name",
+    query: "",
+    pageSize: 10,
+  });
   // res.send('Hello, World!');
+
+  const userId = "user_2vJA3ubDrgSsFc4oErjo7OKehNI";
+
+  // const userId = "user_2vH5z9pq2yFmcHsNaMDoDMfbNjg"; // another user for testing
+
+  // const banUser = await clerkClient.users.banUser(userId, {
+  //   reason: "Violation of terms of service",
+  //   expiresAt: new Date(Date.now() + 30 * 1000), // Ban for 5 minutes
+  // });
+
+  const unbanUser = await clerkClient.users.unbanUser(userId);
+
+  // const userLocked = await clerkClient.users.lockUser(userId, {
+  //   reason: "Suspicious activity detected",
+  //   expiresAt: new Date(Date.now() + 10 * 60 * 1000), // Lock for 10 minutes
+  // });
+
+  // const unlockUser = await clerkClient.users.unlockUser(userId);
 
   const getUser = await clerkClient.users.getUser(
     "user_2upgiJPQCLFbwaKnRCD14yVd1rS"
   );
   const getcount = await clerkClient.users.getCount({
-    query:"Charles"
+    query:""
   });
-  res.json({ data: getUser, total: getcount });
+  res.json({ data, total: getcount });
 });
 
 app.listen(port, () => {
